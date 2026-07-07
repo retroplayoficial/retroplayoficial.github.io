d8db72477d0aa0ab61ff69c166026fb4e2b98b24
retroplayoficial <digitalsantanna941@gmail.com>
2026-07-07 11:41:04 -0300

    atualizacao

---
+++ js/colecao.js
@@ -0,0 +1,23 @@
+document.getElementById("titulo").innerHTML="Mario Collection";
+
+document.getElementById("descricao").innerHTML="Os maiores clássicos do Mario.";
+
+document.getElementById("imagem").innerHTML="🍄 MARIO";
+
+document.getElementById("listaJogos").innerHTML=`
+
+<ul>
+
+<li>Super Mario World</li>
+
+<li>Mario Kart</li>
+
+<li>Mario RPG</li>
+
+<li>Mario All Stars</li>
+
+<li>Yoshi's Island</li>
+
+</ul>
+
+`;
