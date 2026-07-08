d1c58dc1e391df9e399234a62d94dc1d641d05b8
retroplayoficial <digitalsantanna941@gmail.com>
2026-07-08 00:35:01 -0300

    RetroPlay v0.4 - Interface da coleção

---
+++ js/cartucho.js
@@ -0,0 +1,55 @@
+const parametros = new URLSearchParams(window.location.search);
+
+const consoleSelecionado = parametros.get("console");
+const colecaoSelecionada = parametros.get("colecao");
+
+const caminho =
+`../data/${consoleSelecionado}/${colecaoSelecionada}.json`;
+
+fetch(caminho)
+.then(resposta => resposta.json())
+.then(dados => {
+
+document.getElementById("titulo").innerHTML = "RetroPlay";
+
+document.getElementById("subtitulo").innerHTML =
+dados.descricao;
+
+document.getElementById("colecao").innerHTML =
+dados.nome;
+
+let lista = "";
+
+dados.jogos.forEach(jogo => {
+
+lista += `
+
+<div class="jogo">
+
+<div class="nomeJogo">
+
+🎮 ${jogo.nome}
+
+</div>
+
+<a class="botaoJogar" href="#">
+
+▶ Jogar
+
+</a>
+
+</div>
+
+`;
+
+});
+
+document.getElementById("listaJogos").innerHTML = lista;
+
+})
+.catch(() =>{
+
+document.getElementById("listaJogos").innerHTML =
+"Erro ao carregar coleção.";
+
+});
